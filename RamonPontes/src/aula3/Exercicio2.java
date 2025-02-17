e9153a4ebb0d71ba839b173fbdd2b17b26cb8db7
RamonPontes <rgdpontes@yahoo.com>
2025-02-17 14:09:42 -0300

    add: ex2 aula 3

--- RamonPontes/src/aula3/Exercicio2.java
+++ RamonPontes/src/aula3/Exercicio2.java
@@ -10,10 +10,16 @@ package aula3;
  */
 public class Exercicio2 {
     public static void main(String[] args) {
-        double a, b, c = 2, 5, 2;
+        double a = 2, b = 5, c = 2;
         double delta = b * b - 4 * a * c;
         
-        double x1 = (b * -1) - Math.sqrt(delta);
-        double x2 = (b * -1) + Math.sqrt(delta);       
+        double x1 = ((b * -1) - Math.sqrt(delta)) / (2 * a);
+        double x2 = ((b * -1) + Math.sqrt(delta)) / (2 * a);  
+        
+        System.out.println("A = " + a + " B = "+ b + " C = " + c);
+        
+        System.out.println("Delta: " + delta);
+        
+        System.out.println("X1 = " + x1 + " X2 = " + x2);
     }
 }
