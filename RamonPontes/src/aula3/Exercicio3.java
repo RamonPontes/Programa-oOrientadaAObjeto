9b3ea03876e16b99a28738118f47dac623a00d91
RamonPontes <rgdpontes@yahoo.com>
2025-02-17 14:13:33 -0300

    add ex 3 aula 3

--- RamonPontes/src/aula3/Exercicio3.java
+++ RamonPontes/src/aula3/Exercicio3.java
@@ -9,5 +9,20 @@ package aula3;
  * @author info2
  */
 public class Exercicio3 {
-    
+    public static void main(String[] args) {
+        boolean a,b;
+        
+        a = false; b = false;
+        System.out.println(a + " && " + b + " = " + (a && b));
+
+        a = false; b = true;
+        System.out.println(a + " && " + b + " = " + (a && b));
+        
+        a = true; b = true;
+        System.out.println(a + " && " + b + " = " + (a && b));
+
+        a = true; b = false;
+        System.out.println(a + " && " + b + " = " + (a && b));
+
+    }
 }
