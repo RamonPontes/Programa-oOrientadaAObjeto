9e18d8f0ad9842316ca409a7252b689d348ac94e
RamonPontes <rgdpontes@yahoo.com>
2025-02-17 13:58:52 -0300

    init commit

---
+++ RamonPontes/src/aula3/Exercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula3;
+
+/**
+ *
+ * @author info2
+ */
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double precoProduto1 = 3500.00;
+        double precoProduto2 = 249.99;
+        
+        System.out.println("Notebook por: R$" + precoProduto1);
+        System.out.println("SSD por: R$" + precoProduto2);
+    }
+}
---
+++ RamonPontes/src/aula3/Exercicio2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula3;
+
+/**
+ *
+ * @author info2
+ */
+public class Exercicio2 {
+    public static void main(String[] args) {
+        double a, b, c = 2, 5, 2;
+        double delta = b * b - 4 * a * c;
+        
+        double x1 = (b * -1) - Math.sqrt(delta);
+        double x2 = (b * -1) + Math.sqrt(delta);       
+    }
+}
